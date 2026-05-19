e23e0c4ad91b8b89cc8803ea4f3fab67c20b0789
Gvidas09 <kucinskas.gvidas@gmail.com>
2026-05-19 21:50:45 +0300

    failo nuskaitymas

--- src/analyzer.cpp
+++ src/analyzer.cpp
@@ -1,5 +1,32 @@
 #include "analyzer.h"
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
 
 Analyzer::Analyzer() {
     // konstruktorius - konteineriai inicializuojasi automatiškai
+}
+
+void Analyzer::loadText(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open())
+        throw std::runtime_error("Nepavyko atidaryti failo: " + filename);
+
+    std::string line;
+    int lineNum = 1;
+
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::string word;
+
+        while (iss >> word) {
+            // TODO: čia reiks valyti žodžius nuo skyrybos ženklų
+            wordCount_[word]++;
+            crossRef_[word].push_back(lineNum);
+        }
+
+        lineNum++;
+    }
+
+    file.close();
 }
