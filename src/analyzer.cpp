ec71f56bed63064b53b9e0090188b60c7a6e0496
Gvidas09 <kucinskas.gvidas@gmail.com>
2026-05-19 22:40:12 +0300

    skyrybos zenklu salinimas

--- src/analyzer.cpp
+++ src/analyzer.cpp
@@ -2,9 +2,22 @@
 #include <fstream>
 #include <sstream>
 #include <stdexcept>
+#include <algorithm>
+#include <cctype>
 
 Analyzer::Analyzer() {
     // konstruktorius - konteineriai inicializuojasi automatiškai
+}
+
+void Analyzer::cleanWord(std::string& word) {
+    // šalinam skyrybos ženklus iš pradžios ir pabaigos
+    while (!word.empty() && !std::isalpha((unsigned char)word.front()))
+        word.erase(word.begin());
+    while (!word.empty() && !std::isalpha((unsigned char)word.back()))
+        word.pop_back();
+
+    std::transform(word.begin(), word.end(), word.begin(),
+        [](unsigned char c) { return std::tolower(c); });
 }
 
 void Analyzer::loadText(const std::string& filename) {
@@ -20,7 +33,8 @@ void Analyzer::loadText(const std::string& filename) {
         std::string word;
 
         while (iss >> word) {
-            // TODO: čia reiks valyti žodžius nuo skyrybos ženklų
+            cleanWord(word);
+            if (word.empty()) continue;
             wordCount_[word]++;
             crossRef_[word].push_back(lineNum);
         }
